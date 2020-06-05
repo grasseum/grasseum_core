63524e4501e6d4ca231a84804af4b2e51884e506
Pein Freecs <plants.coordinators@gmail.com>
2020-06-05 22:31:31 +0800

    changing the architecture for grasseum, to support modular structure

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
 
-var compt = require("compt");
-var extend_command = require("./extend/command");
-var extend_execute = require("./extend/execute");
-var extend_watch = require("./extend/watch");
+let read_stream = require("./lib/read_stream");
 
-exports.execute_stream = function(argg){
-    return extend_execute(argg)
-}
-exports.terminal_watch = function(argg){
-    extend_watch()
-}
-exports.command_package = function(argg){
-    if(typeof(argg['command']['module']) != "undefined"){
-          
-            extend_command.load_module_command(argg['command'])
-    }else{
-        console.log("Please specify the command `module` you are looking on")
-    }
-}
+exports.readStream = function(conf){
+    let read_arg = read_stream(conf);
+    return read_arg;
+} 
