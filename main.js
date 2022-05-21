2ea4bbb0a858c928f4737fea7495a28b3bc64083
codehyouka <plants.coordinators@gmail.com>
2022-05-21 20:39:16 +0800

    Major enhancement in its feature

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-const stream_event = require("./lib/event/stream_event");
-exports.log = function(){
-    return require("./lib/console");
-}
-
-exports.terminal = function(){
-    return require("./lib/terminal");
-}
+// Const stream_event = require("./lib/event/stream_event");
